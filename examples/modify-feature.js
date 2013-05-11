jorix/OL-Ragbag

commit b467678344952fc2d305b62d0f1ef157f4830a47

Move tools with the point.

lib/Ragbag/Control/ModifyFeature-tools.js
@@ -215,11 +215,17 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
     modified: false,
 
     /**
-     * Property: tools
-     * Array({<OpenLayers.Feature.Vector>}) Tools for dragging rotating resizing
-     *     etc. a feature.
+     * Property: dragTools
+     * Array({<OpenLayers.Feature.Vector>}) Tools for dragging a feature.
      */
-    tools: null,
+    dragTools: null,
+
+    /**
+     * Property: toolbar
+     * Array({<OpenLayers.Feature.Vector>}) Toolbar to rotate resize
+     *     or others a feature.
+     */
+    toolbar: null,
 
     /**
      * Property: mapListeners
@@ -307,7 +313,8 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
         this.layer = layer;
         this.vertices = [];
         this.virtualVertices = [];
-        this.tools = [];
+        this.dragTools = [];
+        this.toolbar = [];
         
         // Configure styles
         var _styles,
@@ -672,19 +679,13 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
         }
         var vertexGeo = vertex.geometry,
             // dragging a real point instead of a vertex, so instantaneous drag
-            isPoint = !vertex._tool,
-            isTool = false,
+            y = pos.lat - vertexGeo.y,
+            x = pos.lon - vertexGeo.x,
+            isRealPoint = !vertex._tool,
+            mustMoveTools = false,
             removeVirtual = true;
-        vertexGeo.move(pos.lon - vertexGeo.x, pos.lat - vertexGeo.y);
+        vertexGeo.move(x, y);
         this.modified = true;
-        /**
-         * Five cases:
-         * 1) dragging a simple point
-         * 2) dragging a virtual vertex
-         * 3) dragging a drag handle
-         * 4) dragging a real vertex
-         * 5) dragging a radius handle
-         */
         if (vertex._index) {
             // dragging a virtual vertex
             vertexGeo.parent.addComponent(vertexGeo, vertex._index);
@@ -694,14 +695,17 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
             vertex.attributes.toolName = 'vertex';
             OpenLayers.Util.removeItem(this.virtualVertices, vertex);
             this.vertices.push(vertex);
-        } else if (vertex._tool === 'vertex'|| isPoint) {
-            // dragging a real vertex or real point
+        } else if (vertex._tool === 'vertex') {
+            // dragging a real vertex
             this.layer.events.triggerEvent("vertexmodified", {
                 vertex: vertexGeo,
                 feature: this.feature,
                 pixel: pixel
-            }); 
-        } else if (vertex._tool === 'point') {
+            });
+        } else if (vertex._tool === 'point' || isRealPoint) {
+            // dragging a point
+            mustMoveTools = true;
+            removeVirtual = false; // if is a 'point' tool then is virtiual.
             var origin = vertexGeo._origin;
             if (origin) {
                 origin.move(pos.lon - origin.x, pos.lat - origin.y);
@@ -713,14 +717,14 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                 feature: this.feature,
                 pixel: pixel
             });
-            removeVirtual = false;
         } else {
             // dragging a tool
-            isTool = true;
-            this.layer.removeFeatures(this.vertices, {silent: true});
-            this.vertices = [];
+            mustMoveTools = true;
+            if (this.vertices.length > 0) {
+                this.layer.removeFeatures(this.vertices, {silent: true});
+                this.vertices = [];
+            }
         }
-        // dragging a radius handle - no special treatment
         if (removeVirtual) {
             if(this.virtualVertices.length > 0) {
                 this.layer.destroyFeatures(this.virtualVertices, {silent: true});
@@ -732,18 +736,28 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
         // keep the vertex on top so it gets the mouseout after dragging
         // this should be removed in favor of an option to draw under or
         // maintain node z-index
-        if (!isPoint) {
+        if (!isRealPoint) {
             this.setVetex(vertex, true);
-            if (isTool) {
-                var tools = this.tools;
-                var renderer = this.layer.renderer;
-                for (var i = tools.length - 1; i >= 0; i--) {
-                    renderer.locked = (i > 0); // to remove some flashing using Canvas
-                    this.layer.drawFeature(tools[i]);
-                }
-            } else {
-                this.layer.drawFeature(vertex);
+        }
+        if (mustMoveTools) {
+            this.moveTools(this.toolbar, x, y);
+            if (vertex._tool === 'drag') {
+                this.moveTools(this.dragTools, x, y);
             }
+        } else {
+            this.layer.drawFeature(vertex);
+        }
+    },
+    
+    /**
+     * Method: moveTools
+     */
+    moveTools: function(toolArray, x, y) {
+        var movePoint = OpenLayers.Geometry.Point.prototype.move,
+            layer = this.layer;
+        for (var i = 0, len = toolArray.length; i < len; i++) {
+            movePoint.call(toolArray[i].geometry, x, y);
+            layer.drawFeature(toolArray[i]);
         }
     },
     
@@ -801,9 +815,13 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
      * Method: removeTools
      */
     removeTools: function() {
-        if (this.tools.length > 0) {
-            this.layer.destroyFeatures(this.tools, {silent: true});
-            this.tools = [];
+        if (this.dragTools.length > 0) {
+            this.layer.destroyFeatures(this.dragTools, {silent: true});
+            this.dragTools = [];
+        }
+        if (this.toolbar.length > 0) {
+            this.layer.destroyFeatures(this.toolbar, {silent: true});
+            this.toolbar = [];
         }
     },
 
@@ -1044,22 +1062,14 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                 center.lon, center.lat
             ),
             _self = this,
-            _tools = [],
+            _dragTools = [],
+            _toolbar = [],
             // no scope
-            addTool = function(toolName, x, y, toolMove) {
+            addTool = function(toolArray, toolName, x, y, toolMove) {
                 var toolGeometry = new OpenLayers.Geometry.Point(x, y);
                 toolGeometry.move = toolMove;
-                var tool = _self.createVertex(toolName, toolGeometry);
-                _tools.push(tool);
-                return tool;
+                toolArray.push(_self.createVertex(toolName, toolGeometry));
             },
-            _movePoint = OpenLayers.Geometry.Point.prototype.move,
-            _moveTool = function(_toolPos, x, y) {
-                for (var i = 0, len = _toolPos.length; i < len; i++) {
-                    _movePoint.call(_toolPos[i].geometry, x, y);
-                }
-            },
-            _toolPosBounds = [],
             buttonSize = this.buttonSize,
             resolution = this.map.getResolution(),
             _sizeX = buttonSize.w * resolution,
@@ -1067,28 +1077,24 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
             _posX = bounds.right,
             _posY = bounds.top + _sizeY,
             addToolBounds = function(toolName, p_dxAction) {
-                _toolPosBounds.push(
-                    addTool(toolName,
-                        _posX,
-                        _posY,
-                        function(x, y) {
-                            _moveTool(_toolPosBounds, x, y);
-                            var dx1 = this.x - _originGeometry.x;
-                            var dy1 = this.y - _originGeometry.y;
-                            var dx0 = dx1 - x;
-                            var dy0 = dy1 - y;
-                            p_dxAction(dx1, dy1, dx0, dy0);
-                        }
-                    )
+                addTool(_toolbar, toolName,
+                    _posX,
+                    _posY,
+                    function(x, y) {
+                        var dx1 = this.x - _originGeometry.x;
+                        var dy1 = this.y - _originGeometry.y;
+                        var dx0 = dx1 - x;
+                        var dy0 = dy1 - y;
+                        p_dxAction(dx1, dy1, dx0, dy0);
+                    }
                 );
                 _posX -= _sizeX;
             };
         // declare tools
         if (!isLikeAPoint) {
             if (mode & MODES.DRAG) {
-                addTool("drag", center.lon, center.lat, 
+                addTool(_dragTools, "drag", center.lon, center.lat,
                     function(x, y) {
-                        _moveTool(_tools, x, y);
                         _geometry.move(x, y);
                     }
                 );
@@ -1127,8 +1133,10 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
         if (deleteTool) {
             addToolBounds("delete", function() {});
         }
-        this.layer.addFeatures(_tools, {silent: true});
-        this.tools = _tools;
+        this.layer.addFeatures(_dragTools, {silent: true});
+        this.dragTools = _dragTools;
+        this.layer.addFeatures(_toolbar, {silent: true});
+        this.toolbar = _toolbar;
         return mode;
     },
 

commit 1421c7235a325d995bb1868c54240c291bd8d539

Allows add custom tools
And no longer creates the attribute toolName for the tools.

NOTE: This change also fixes a regression produced
by b467678344952fc2d305b62d0f1ef157f4830a47 (now dx0 is dx1 and
vice versa so tool is moved after the feature is modified)

examples/modify-feature.html
@@ -4,8 +4,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="lang" content="en">
     <meta name="author" content="Jorix">
-    <meta name="description" content="Usage example of ModifyFeature control for editing and deleting vector features.">
-    <meta name="keywords" content="OpenLayers, vertices, draw, drawing, modify, delete">
+    <meta name="description" content="Usage example of ModifyFeature control for editing deleting and modify attributes of vector features.">
+    <meta name="keywords" content="OpenLayers, vertices, draw, drawing, modify, delete, attributes">
     <link rel="stylesheet" href="http://jorix.github.com/OL-Ragbag/examples/github-pages/forkme_banner.css" type="text/css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -22,14 +22,20 @@
     </style>
 
     <script src="http://www.openlayers.org/dev/OpenLayers.js"></script>
+    <!-- To improve editing by separate and add delete and custom tools and allows undo by <esc-key> -->
     <script src="../lib/Ragbag/Control/ModifyFeature-tools.js"></script>
+    <!-- To improve drawing tools to allow undo by <esc-key> -->
+    <script src="../lib/Ragbag/Handler/Path-patch.js"></script>
 
   </head>
   <body>
-    <h1 id="title">Example of OpenLayers to Modify and Delete features</h1>
+    <h1 id="title">Example of OpenLayers to edit delete and modify attributes of vector features.</h1>
     <a id="forkme_banner" href="https://github.com/jorix/OL-Ragbag#improve-drawing-and-editing-tools-adding-behaviors">View on GitHub</a>
     <div id="tags">vertices, draw, drawing, modify, delete</div>
-    <div id="shortdesc">Example of ModifyFeature control for editing and deleting vector features.</div>
+    <div id="shortdesc">
+        <p>Is obtained an improvement of usability by separating the tools and allowing a different icon for each tool, and empowers to use &lt;esc-key&gt; to cancel the current drag and remove last vertex added.</p>
+        <p>Also allows a delete tool and add custom tools (eg to modify attributes: see points on this example, or close a line as a ring: see lines)</p>
+    </div>
     <div id="map" class="smallmap"></div>
     <div id="controls">
         <ul id="controlToggle">

examples/modify-feature.js
@@ -7,7 +7,15 @@ OpenLayers.Layer.Vector.prototype.renderers = renderer ?
                                     [renderer] :
                                     OpenLayers.Layer.Vector.prototype.renderers;
 
-OpenLayers.Feature.Vector.style['default']['strokeWidth'] = '3';
+OpenLayers.Util.extend(OpenLayers.Feature.Vector.style['default'], {
+    strokeWidth: 3,
+    graphicName: 'triangle',
+    pointRadius: '${radius}',
+    rotation: '${angle}'
+});
+OpenLayers.Util.extend(OpenLayers.Feature.Vector.style['select'], {
+    pointRadius: '${radius}'
+});
 
 // Create Objects
 // --------------
@@ -27,10 +35,7 @@ if (window.console && window.console.log) {
 var vectorLayer = new OpenLayers.Layer.Vector('Vector Layer');
 vectorLayer.events.on({
     'beforefeaturemodified': reportEvent,
-    'featuremodified': function(e) {
-        e.feature.state = OpenLayers.State.UPDATE;
-        reportEvent(e);
-    },
+    'featuremodified': reportEvent,
     'afterfeaturemodified': reportEvent,
     'beforefeatureremoved': reportEvent,
     'featureremoved': reportEvent,
@@ -38,6 +43,12 @@ vectorLayer.events.on({
     'sketchmodified': reportEvent,
     'sketchstarted': reportEvent,
     'sketchcomplete': reportEvent
+});
+// to ensure that the points have radius
+vectorLayer.events.on({
+    'beforefeatureadded': function(e) {
+        e.feature.attributes.radius = 6;
+    }
 });
 
 // Create and show the map
@@ -64,7 +75,44 @@ var controls = {
         eventListeners: {
             'beforefeaturedeleted': reportEvent,
             'featuredeleted': reportEvent
-        }
+        },
+        tools: [ // custom tools
+        { 
+            // to rotate the "angle" attribute of a ponit by steps of 15 degrees
+            geometryTypes: ['OpenLayers.Geometry.Point'],
+            dragAction: function(feature, initialAtt, escale, rotation) {
+                var angle = ((initialAtt.angle || 0) - rotation) % 360;
+                // force steps of 15 degrres
+                angle = Math.floor(angle / 15) * 15;
+                feature.attributes.angle = angle;
+            },
+            style: OpenLayers.Control.ModifyFeature_styles.rotate
+        }, {
+            // to resize the pointRadius.
+            geometryTypes: ['OpenLayers.Geometry.Point'],
+            dragAction: function(feature, initialAtt, escale, rotation) {
+                var radius = (initialAtt.radius || 6) * escale;
+                feature.attributes.radius = Math.max(6, radius);
+            },
+            style: OpenLayers.Control.ModifyFeature_styles.resize
+        }, {
+            // to close a lineString as a ring
+            geometryTypes: ['OpenLayers.Geometry.LineString'],
+            pressingAction: function(feature) {
+                var geometry = feature.geometry;
+                geometry.addComponent(geometry.components[0].clone());
+            },
+            style: {
+                label:'ring',
+                title: 'press to close as a ring',
+                cursor: "pointer",
+                fontSize: '8px',
+                fontColor: '#222',
+                pointRadius: 10,
+                fillColor: '#cccccc',
+                strokeColor: '#444444'
+            }
+        }]
     })
 };
 // add this controls to the map

lib/Ragbag/Control/ModifyFeature-tools.js
@@ -96,6 +96,9 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
      * APIProperty: escapeCode
      * {Integer} Keycode for cancel a vertex drag.  Set to null to
      *     disable cancel vertex and tool drad by keypress.  Default is 27.
+     *
+     * NOTE: By canceling the tools DRAG, DEFORM or ROTATE can be obtained a
+     *     geometry slightly different from starting (not visually perceptible)
      */
     escapeCode: 27,
 
@@ -105,6 +108,18 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
      *     <OpenLayers.Control.ModifyFeature_styles>.
      */
     styles: null,
+    
+    /**
+     * APIProperty: tools
+     * Array({Object}) Custom tools to use with this control. Each tool needs a
+     *     function on a key named 'dragAction' (4 arguments: feature,
+     *     initialAttributes, scale {Float}, rotation {Float})
+     *     or 'pressingAction' (only feature argument), and could
+     *     have additional keys: 'style' {Object} (with a default and
+     *     select keys) and 'geometryTypes' Array({String}) (see usage on
+     *     <geometryTypes>)
+     */
+    tools: null,
 
     /**
      * Property: buttonSize
@@ -223,7 +238,7 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
     /**
      * Property: toolbar
      * Array({<OpenLayers.Feature.Vector>}) Toolbar to rotate resize
-     *     or others a feature.
+     *     or others actions on a feature.
      */
     toolbar: null,
 
@@ -356,6 +371,12 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                     _styles.vvertex.strokeOpacity = 0.3;
                 }
             }
+            var tools = this.tools;
+            if (tools) {
+                for (var i = 0, len = tools.length; i < len; i++) {
+                    _styles['custom_' + i] = tools[i].style;
+                }
+            }
             this._styles = _styles; // only needed to debug
         };
         this.setStyles(options.styles);
@@ -475,12 +496,11 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
      * Method: createVertex
      * Take care of things that are not handled in superclass.
      */
-    createVertex: function(toolName, geometry, highlight) {
-        var vertex = new OpenLayers.Feature.Vector(geometry, {
-            toolName: toolName
-        });
+    createVertex: function(toolName, pressingAction, geometry, highlight) {
+        var vertex = new OpenLayers.Feature.Vector(geometry);
         vertex._sketch = true;
         vertex._tool = toolName;
+        vertex._pressingAction = pressingAction;
         this.setVetex(vertex, highlight);
         return vertex;
     },
@@ -634,7 +654,7 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
         var isPoint =
                     feature.geometry.CLASS_NAME === 'OpenLayers.Geometry.Point';
         if (!feature._sketch && isPoint && this.virtualVertices.length) {
-            // if exists drag the virtual point instead of the real point
+            // if exists then drag the virtual point instead of the real point
             feature = this.virtualVertices[0];
         }
         if (feature._sketch || isPoint) {
@@ -645,7 +665,7 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                 pixx: pixel.x,
                 pixy: pixel.y
             };
-            // feature is a vertex tool or point
+            // feature is a tool a vertex or a real point
             this.vertex = feature;
             this.handlers.drag.stopDown = true;
         }
@@ -660,8 +680,17 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
      * pixel - {<OpenLayers.Pixel>} Pixel location of the mouse event.
      */
     pressVertex: function(vertex, pixel) {
-        if (vertex._tool === "delete") {
-            this.deleteFeature(this.feature);
+        if (!vertex._pressingAction) {
+            return;
+        }
+        var feature = this.feature;
+        if (vertex._tool === 'delete') {
+            this.deleteFeature(feature);
+        } else {
+            vertex._pressingAction(feature);
+            feature.layer.drawFeature(feature, this.standalone ? undefined :
+                                        'select');
+            this.dragComplete();
         }
     },
 
@@ -674,10 +703,10 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
      * pixel - {<OpenLayers.Pixel>} Pixel location of the mouse event.
      */
     dragVertex: function(vertex, pos, pixel) {
-        var toolName = vertex._tool;
-        if (toolName === "delete") {
+        if (vertex._pressingAction) {
             return;
         }
+        var toolName = vertex._tool;
         var vertexGeo = vertex.geometry,
             layer = this.layer,
             feature = this.feature,
@@ -696,7 +725,6 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
             delete vertex._index;
             toolName = 'vertex';
             vertex._tool = toolName;
-            vertex.attributes.toolName = toolName;
             OpenLayers.Util.removeItem(this.virtualVertices, vertex);
             this.vertices.push(vertex);
         } else if (toolName === 'vertex') {
@@ -863,7 +891,6 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
         if (cancelDrag && vStart) {
             vertex = this.vertex;
             dragHandler.deactivate();
-            this.vStart = null;
             dragHandler.activate();
             if (vStart._tool === 'vvertex') {
                 delVertex = true;
@@ -929,7 +956,7 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
             } else {
                 feature.state = OpenLayers.State.DELETE;
                 layer.drawFeature(feature);
-                feature.layer.events.triggerEvent(
+                layer.events.triggerEvent(
                     'afterfeaturemodified', {
                         feature: feature,
                         modified: true
@@ -967,9 +994,9 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                                                    .destroy.apply(this, arguments);
                     this._origin = null;
                 };
-                _virtualVertices.push(control.createVertex('point', p));
+                _virtualVertices.push(control.createVertex('point', false, p));
             } else {
-                _vertices.push(control.createVertex('point', geometry));
+                _vertices.push(control.createVertex('point', false, geometry));
             }
         };
         function collectComponentVertices(geometry) {
@@ -994,7 +1021,8 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                     if(component.CLASS_NAME == "OpenLayers.Geometry.Point") {
                         if (isLine) {
                             _vertices.push(
-                                     control.createVertex('vertex', component));
+                                     control.createVertex(
+                                                   'vertex', false, component));
                         } else {
                             _pushPoint(component)
                         }
@@ -1009,6 +1037,7 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                         var prevVertex = components[i],
                             nextVertex = components[i + 1];
                         var point = control.createVertex('vvertex',
+                            false,
                             new OpenLayers.Geometry.Point(
                                 (prevVertex.x + nextVertex.x) / 2,
                                 (prevVertex.y + nextVertex.y) / 2
@@ -1035,7 +1064,8 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
     collectTools: function() {
         var MODES = OpenLayers.Control.ModifyFeature,
             mode = this.mode,
-            _geometry = this.feature.geometry,
+            _feature = this.feature,
+            _geometry = _feature.geometry,
             bounds = _geometry.getBounds(),
             isLikeAPoint = bounds.left === bounds.right &&
                            bounds.top === bounds.bottom,
@@ -1059,10 +1089,12 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
             _dragTools = [],
             _toolbar = [],
             // no scope
-            addTool = function(toolArray, toolName, x, y, toolMove) {
+            addTool = function(
+                          toolArray, toolName, x, y, pressingAction, dragAction) {
                 var toolGeometry = new OpenLayers.Geometry.Point(x, y);
-                toolGeometry.move = toolMove;
-                toolArray.push(_self.createVertex(toolName, toolGeometry));
+                toolGeometry.move = dragAction;
+                toolArray.push(
+                    _self.createVertex(toolName, pressingAction, toolGeometry));
             },
             buttonSize = this.buttonSize,
             resolution = this.map.getResolution(),
@@ -1070,31 +1102,69 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
             _sizeY = buttonSize.h * resolution,
             _posX = bounds.right,
             _posY = bounds.top + _sizeY,
-            addToolBounds = function(toolName, p_dxAction) {
-                addTool(_toolbar, toolName,
-                    _posX,
-                    _posY,
+            addToToolbar = function(toolName, pressingAction, p_dxAction) {
+                addTool(_toolbar, toolName, _posX, _posY, pressingAction,
                     function(x, y) {
-                        var dx1 = this.x - _originGeometry.x;
-                        var dy1 = this.y - _originGeometry.y;
-                        var dx0 = dx1 - x;
-                        var dy0 = dy1 - y;
+                        // values before move the vertex or the tool
+                        var dx0 = this.x - _originGeometry.x,
+                            dy0 = this.y - _originGeometry.y,
+                            dx1 = dx0 + x,
+                            dy1 = dy0 + y;
                         p_dxAction(dx1, dy1, dx0, dy0);
                     }
                 );
                 _posX -= _sizeX;
             };
-        // declare tools
+        // collect custom tools
+        var tools = this.tools;
+        if (tools) {
+            var _initialAttibutes =
+                                OpenLayers.Util.extend({}, _feature.attributes),
+                _cumulativeAngle = 0,
+                _cumulativeScale = 1,
+                addCustomTool = function(toolName, pressingAction, dragAction) {
+                    addToToolbar(
+                        toolName,
+                        pressingAction,
+                        function(dx1, dy1, dx0, dy0) {
+                            var a0 = Math.atan2(dy0, dx0),
+                                a1 = Math.atan2(dy1, dx1),
+                                angle = (a1 - a0) * 180 / Math.PI,
+                                l0 = Math.sqrt((dx0 * dx0) + (dy0 * dy0)),
+                                l1 = Math.sqrt((dx1 * dx1) + (dy1 * dy1)),
+                                scale = l1 / l0;
+                            _cumulativeAngle += angle,
+                            _cumulativeScale *= scale,
+                            dragAction(_feature, _initialAttibutes,
+                                Math.round(100000 * _cumulativeScale) / 100000,
+                                Math.round(1000 * _cumulativeAngle) / 1000
+                            );
+                        }
+                    );
+                },
+                feoGeoClassName = _geometry.CLASS_NAME;
+            for (var i = 0, len = tools.length; i < len; i++) {
+                var tool = tools[i],
+                    geometryTypes = tool.geometryTypes;
+                if (!geometryTypes ||
+                    OpenLayers.Util.indexOf(
+                                       geometryTypes, feoGeoClassName) !== -1) {
+                    addCustomTool('custom_' + i,
+                                          tool.pressingAction, tool.dragAction);
+                }
+            }
+        }
+        // collect standart tools
         if (!isLikeAPoint) {
             if (mode & MODES.DRAG) {
-                addTool(_dragTools, "drag", center.lon, center.lat,
+                addTool(_dragTools, "drag", false, center.lon, center.lat,
                     function(x, y) {
                         _geometry.move(x, y);
                     }
                 );
             }
             if (mode & MODES.ROTATE) {
-                addToolBounds("rotate",
+                addToToolbar("rotate", false,
                     function(dx1, dy1, dx0, dy0) {
                         var a0 = Math.atan2(dy0, dx0);
                         var a1 = Math.atan2(dy1, dx1);
@@ -1105,7 +1175,7 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                 );
             }
             if (mode & MODES.RESIZE) {
-                addToolBounds("resize",
+                addToToolbar("resize", false,
                     function(dx1, dy1, dx0, dy0) {
                         var l0 = Math.sqrt((dx0 * dx0) + (dy0 * dy0)),
                             l1 = Math.sqrt((dx1 * dx1) + (dy1 * dy1)),
@@ -1115,7 +1185,7 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
                 );
             }
             if (mode & MODES.DEFORM) {
-                addToolBounds("deform",
+                addToToolbar("deform", false,
                     function(dx1, dy1, dx0, dy0) {
                         var scale = dy1 / dy0,
                             ratio = (dx1 / dx0) / scale;
@@ -1125,8 +1195,9 @@ OpenLayers.Control.ModifyFeature = OpenLayers.Class(OpenLayers.Control, {
             }
         }
         if (deleteTool) {
-            addToolBounds("delete", function() {});
+            addToToolbar("delete", true, function() {});
         }
+
         this.layer.addFeatures(_dragTools, {silent: true});
         this.dragTools = _dragTools;
         this.layer.addFeatures(_toolbar, {silent: true});
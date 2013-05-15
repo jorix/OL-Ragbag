bd8daae4b894ab20911b98233680e4ed98198090
Xavier Mamano <xavier.mamano@gmail.com>
2013-05-15 16:45:17 +0200

    Custom CSS for docs

--- doc/_scripts/documentation.cmd
+++ doc/_scripts/documentation.cmd
@@ -1,5 +1,5 @@
-call naturaldocs -i ../../lib -hl All -s Default OL -o HTML ../Ragbag/all -p ../Ragbag/all/_config
-call naturaldocs -i ../../lib -hl All -s Default OL -o HTML ../Ragbag/api -p ../Ragbag/api/_config
-call naturaldocs -i ../../dev -hl All -s Default OL -o HTML ../dev-releaseEnvirontment -p ../dev-releaseEnvirontment/_config
+call naturaldocs -i ../../lib -hl All -s Default ../../../_scripts/jorix -o HTML ../Ragbag/all -p ../Ragbag/all/_config
+call naturaldocs -i ../../lib -hl All -s Default ../../../_scripts/jorix -o HTML ../Ragbag/api -p ../Ragbag/api/_config
+call naturaldocs -i ../../dev -hl All -s Default ../../_scripts/jorix -o HTML ../dev-releaseEnvirontment -p ../dev-releaseEnvirontment/_config
 
 pause
